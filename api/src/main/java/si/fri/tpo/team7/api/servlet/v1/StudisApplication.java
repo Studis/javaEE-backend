99f6d2d21a6267dd3a2f2c40163572470a6b84d9
Domen Gašperlin <domen.gasperlin@gmail.com>
2018-04-11 00:58:34 +0200

    Healthcheck and CORS

--- api/src/main/java/si/fri/tpo/team7/api/servlet/v1/StudisApplication.java
+++ api/src/main/java/si/fri/tpo/team7/api/servlet/v1/StudisApplication.java
@@ -1,11 +1,14 @@
 package si.fri.tpo.team7.api.servlet.v1;
 
+import com.kumuluz.ee.cors.annotations.CrossOrigin;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
 @ApplicationPath("/v1")
+@CrossOrigin
 public class StudisApplication extends Application {
     @Override
     public Set<Class<?>> getClasses()
