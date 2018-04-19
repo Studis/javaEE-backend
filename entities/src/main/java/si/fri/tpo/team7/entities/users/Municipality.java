53f49e5e01265da2564d72bb5090a131b8478301
Jaka Kordez <jaka.kordez@gmail.com>
2018-04-19 11:01:33 +0200

    Added municipality.

---
+++ entities/src/main/java/si/fri/tpo/team7/entities/users/Municipality.java
@@ -0,0 +1,20 @@
+package si.fri.tpo.team7.entities.users;
+
+import si.fri.tpo.team7.entities.Register;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class Municipality extends Register {
+    @Column(name="name")
+    protected String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
