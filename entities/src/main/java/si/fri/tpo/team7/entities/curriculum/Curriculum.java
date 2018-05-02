9a5580b9cb0a50e8a473ea2224f6c1abf2335007
Jaka Kordez <jaka.kordez@gmail.com>
2018-05-02 11:35:39 +0200

    Many to many relation in optional courses.

--- entities/src/main/java/si/fri/tpo/team7/entities/curriculum/Curriculum.java
+++ entities/src/main/java/si/fri/tpo/team7/entities/curriculum/Curriculum.java
@@ -28,4 +28,34 @@ public class Curriculum extends BaseEntity {
     @JsonIgnore
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "curriculum")
     private List<Enrollment> enrollments;
+
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "curriculum")
+    private List<ObligatoryCourse> obligatoryCourses;
+
+    @JsonIgnore
+    @ManyToMany(cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+    })
+    @JoinTable(name = "professionaloptionalcourse_curriculum",
+            joinColumns = @JoinColumn(name = "curriculums_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "professionaloptionalcourse_id", referencedColumnName = "id")
+    )
+    private List<ProfessionalOptionalCourse> professionalOptionalCourses;
+
+    @JsonIgnore
+    @ManyToMany(cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+    })
+    @JoinTable(name = "generaloptionalcourse_curriculum",
+            joinColumns = @JoinColumn(name = "curriculums_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "generaloptionalcourse_id", referencedColumnName = "id")
+    )
+    private List<GeneralOptionalCourse> generalOptionalCourses;
+
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "curriculum")
+    private List<Module> modules;
 }
